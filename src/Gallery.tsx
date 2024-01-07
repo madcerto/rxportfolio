8405195e182a3dc916b20544221184c73ad9fdd1
Zaryab <madscientist2854@gmail.com>
2024-01-07 03:00:30 -0500

    use unscaled image in popup

--- src/Gallery.tsx
+++ src/Gallery.tsx
@@ -11,13 +11,24 @@ export default function Gallery() {
       w: 420,
     }
   }))
+  const originals = Object.values(import.meta.glob('/src/assets/Film Scans/Lightroom-ed/*.{png,jpg,jpeg,PNG,JPEG,JPG}', {
+    eager: true,
+    import: "default",
+    query: {
+      format: "webp",
+    }
+  }))
 
   const [popupSrc, setPopupSrc] = useState(null)
 
   return <div className="flex-1 h-screen overflow-auto">
     <div className="pt-32 lg:columns-3 columns-2 gap-0">
-      {previews.map(img => 
-        <img className="overflow-auto lg:p-3 p-1 hover:cursor-pointer" onClick={()=>setPopupSrc(img)} src={img}/>
+      {previews.map((img,i) => 
+        <img
+          className="overflow-auto lg:p-3 p-1 hover:cursor-pointer"
+          onClick={()=>setPopupSrc(originals[i])}
+          src={img}
+        />
       )}
       <ImagePopup src={popupSrc} onClose={()=>setPopupSrc(null)}/>
     </div>
