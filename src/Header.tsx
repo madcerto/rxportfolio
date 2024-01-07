70b289f7a70a5b73c3b81a4c4bc3e3bad14c1420
Zaryab <madscientist2854@gmail.com>
2024-01-07 09:04:50 -0500

    convert constants to config json that allows aliases for albums

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,15 +1,18 @@
 import {Link} from 'react-router-dom'
-import {TITLE, DESCRIPTION, ALBUMS} from '../constants.ts';
+import config from '/config.json'
 
 export default function Header() {
   return (
     <div className="lg:w-80 w-64 p-8 pt-16">
-      <p className="font-noto font-bold text-2xl pb-8">{TITLE}</p>
+      <p className="font-noto font-bold text-2xl pb-8">{config.title}</p>
     
       <div className="font-lato text-[#6b747a]">
-        <p dangerouslySetInnerHTML={{__html: DESCRIPTION}} />
+        <p dangerouslySetInnerHTML={{__html: config.description}} />
         <ul className="pt-8">
-          {ALBUMS.map(album => <li><Link to={"/"+album}>{album}</Link></li>)}
+          {config.albums.map(album => {
+            if (typeof album === "string") return <li><Link to={"/"+album}>{album}</Link></li>
+            else return <li><Link to={"/"+album[1]}>{album[0]}</Link></li>
+          })}
         </ul>
       </div>
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -8,14 +8,14 @@ import {
   Route,
   Navigate
 } from "react-router-dom";
-import {ALBUMS} from '../constants.ts'
+import config from '../config.json'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route index element={<Navigate to={"/"+ALBUMS[0]} />} />
+        <Route index element={<Navigate to={"/"+config.albums[0]} />} />
         <Route path="/:album" element={<App />} />
       </Routes>
     </BrowserRouter>
