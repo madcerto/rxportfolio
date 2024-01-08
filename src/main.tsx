41936f8c67ccdcf99e306aab912b315f4963a7b7
Zaryab <madscientist2854@gmail.com>
2024-01-08 03:47:58 -0500

    make all unknown routes redirect to default album

--- src/main.tsx
+++ src/main.tsx
@@ -10,17 +10,15 @@ import {
 import config from '../config.json'
 import './index.css'
 
-console.log("/rxportfolio/" + ((typeof config.albums[0] === "string")
-            ? config.albums[0] : config.albums[0][1]))
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path={["/", "/rxportfolio"]} element={<Navigate to={
+        <Route path="/rxportfolio/:album" element={<App />} />
+        <Route path="*" element={<Navigate to={
           "/rxportfolio/" + ((typeof config.albums[0] === "string")
             ? config.albums[0] : config.albums[0][1])
         } />} />
-        <Route path="/rxportfolio/:album" element={<App />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
