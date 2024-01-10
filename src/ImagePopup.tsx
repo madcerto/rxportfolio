808a97d7ef8b47977078ce0cd845851c76fe283d
Zaryab <madscientist2854@gmail.com>
2024-01-09 23:39:32 -0500

    fix image popup stretched on mobile

--- src/ImagePopup.tsx
+++ src/ImagePopup.tsx
@@ -5,11 +5,11 @@ export default function ImagePopup(props: {src?: string|null, onClose?: any}) {
     <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
       <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
       <div className="fixed inset-0 z-10 overflow-y-auto" onClick={props.onClose}>
-        <div className="flex h-full items-end justify-center p-4 text-center sm:items-center sm:p-6">
+        <div className="flex h-full justify-center p-4 text-center items-center sm:p-6">
             <div className="fixed w-12 m-4 top-0 right-0 text-black hover:cursor-pointer" onClick={props.onClose}>
               <img src={x} />
             </div>
-            <img src={props.src} className="h-full" onClick={(e)=>e.stopPropagation()}/>
+            <img src={props.src} className="max-h-full" onClick={(e)=>e.stopPropagation()}/>
         </div>
       </div>
     </div>
